44a1ed47aaf6857dd04e9bd5c9091f9d0759492f
Liam Doyle <limegorilla@gmail.com>
2021-03-11 13:37:37 +0000

    .

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // Connects to middle image views on main screen
+    @IBOutlet weak var diceImageViewLeft: UIImageView!
+    @IBOutlet weak var diceImageViewRight: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        changeDiceView()
+    
     }
-
-
+    
+    // Array of dice images
+    let imagesArray = [ #imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix") ]
+    
+    // Function to change images
+    func changeDiceView() {
+        var randomNumber = Int.random(in: 0...5) // Only goes up to 5 because of base 0
+        diceImageViewRight.image = imagesArray[randomNumber] // Sets selected UIImageView to argument number
+        randomNumber = Int.random(in: 0...5) // Reset number
+        diceImageViewLeft.image = imagesArray[randomNumber] // Sets selected UIImageView to argument number
+    }
+    
+    
+    // Button trigger
+    @IBAction func rollButtonPress(_ sender: UIButton) {
+        changeDiceView()
+    }
+    
 }
 
